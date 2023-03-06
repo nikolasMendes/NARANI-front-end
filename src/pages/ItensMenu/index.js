e19df222b5f48887b926d65eebaf5500be7e72fd
nayra <guerreironayra@gmail.com>
2023-03-06 15:39:22 -0300

    link para pag de edit menu

--- src/pages/ItensMenu/index.js
+++ src/pages/ItensMenu/index.js
@@ -39,7 +39,9 @@ export function ItensMenu() {
           );
         })}
         {loggedInUser && loggedInUser.user.role === "ADMIN" ? (
-          <button>Gerenciar</button>
+          <Link to="/edit/:id">
+            <button>Gerenciar</button>
+          </Link>
         ) : null}
       </>
     </>
