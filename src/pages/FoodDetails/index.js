7c9325fc5c187f7aa73b5d248d7920e87ff96c9d
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-03-08 10:32:42 -0300

    css da FoodDetails

--- src/pages/FoodDetails/index.js
+++ src/pages/FoodDetails/index.js
@@ -33,14 +33,48 @@ export function FoodDetails(props) {
     <div>
       {!isLoading && (
         <>
-          <h3>{food.prato}</h3>
-          <img alt={food.prato} src={food.imagem} />
-          <p> Descrição: {food.descrição}</p>
-          <p>Porção: {food.quantidade}</p>
-          <p>Serve: {food.serve}</p>
-          <p>Tempo de preparo: {food.preparo}</p>
-          <p>Calorias: {food.calorias}</p>
-          <button onClick={handleSubmit}>Fazer Pedido</button>
+          <div className="mx-auto my-8 max-w-3xl border-4 border-slate-700 flex flex-col rounded-3xl">
+            <img
+              alt={food.prato}
+              src={food.imagem}
+              className="rounded-xl border-2 border-slate-700 "
+            />
+            <h1 className="pt-3 text-center text-3xl font-bold">
+              {food.prato}
+            </h1>
+            <div className="p-4">
+              <p className="pb-4">
+                <span className="font-semibold"> Descrição:</span>{" "}
+                {food.descrição}
+              </p>
+
+              <div className="flex justify-between">
+                <p>
+                  <span className="font-semibold">Tempo de preparo: </span>
+                  {food.preparo}
+                </p>
+                <p className=" pr-80">
+                  <span className="font-semibold">Calorias:</span>{" "}
+                  {food.calorias}
+                </p>
+              </div>
+              <div className="flex justify-between  pt-3">
+                <p>
+                  <span className="font-semibold">Porção: </span>
+                  {food.quantidade}
+                </p>
+                <p className=" pr-96">
+                  <span className="font-semibold">Serve: </span>
+                  {food.serve}
+                </p>
+              </div>
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="mx-auto w-1/4 mb-8 bg-black text-white p-3 font-bold rounded-full hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-7  00  ">
+              Fazer Pedido
+            </button>
+          </div>
         </>
       )}
     </div>
