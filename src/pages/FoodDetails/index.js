5435b7577adfde5c28d51411219df61c1ba80fd8
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-03-08 18:22:17 -0300

    padronização css createfood, editfood e fooddetails

--- src/pages/FoodDetails/index.js
+++ src/pages/FoodDetails/index.js
@@ -33,47 +33,43 @@ export function FoodDetails(props) {
     <div>
       {!isLoading && (
         <>
-          <div className="mx-auto my-8 max-w-3xl border-4 border-slate-700 flex flex-col rounded-3xl">
-            <img
-              alt={food.prato}
-              src={food.imagem}
-              className="rounded-xl border-2 border-slate-700 "
-            />
-            <h1 className="pt-3 text-center text-3xl font-bold">
-              {food.prato}
-            </h1>
-            <div className="p-4">
-              <p className="pb-4">
-                <span className="font-semibold"> Descrição:</span>{" "}
-                {food.descrição}
-              </p>
+          <div class="flex min-h-full items-center  bg-[#F9E4D4] h-screen pb-36">
+            <div className="mx-auto max-w-3xl border-4 border-slate-700 flex flex-col rounded-3xl bg-[#e09e6e] shadow-2xl ">
+              <img
+                alt={food.prato}
+                src={food.imagem}
+                className="rounded-xl border-2 border-slate-700 max-h-96"
+              />
+              <h1 className="pt-3 text-center text-3xl font-bold">
+                {food.prato}
+              </h1>
+              <div className="p-4">
+                <p className="pb-4">
+                  <span className="font-semibold"> Descrição:</span>{" "}
+                  {food.descrição}
+                </p>
 
-              <div className="flex justify-between">
-                <p>
-                  <span className="font-semibold">Tempo de preparo: </span>
-                  {food.preparo}
-                </p>
-                <p className=" pr-80">
-                  <span className="font-semibold">Calorias:</span>{" "}
-                  {food.calorias}
-                </p>
+                <div className="flex justify-between pb-6">
+                  <p>
+                    <span className="font-semibold">Tempo de preparo: </span>
+                    {food.preparo}
+                  </p>
+                  <p className="pr-7">
+                    <span className="font-semibold">Calorias:</span>{" "}
+                    {food.calorias}
+                  </p>
+                  <p className="pr-12">
+                    <span className="font-semibold">Porção: </span>
+                    {food.quantidade} porções
+                  </p>
+                </div>
               </div>
-              <div className="flex justify-between  pt-3">
-                <p>
-                  <span className="font-semibold">Porção: </span>
-                  {food.quantidade}
-                </p>
-                <p className=" pr-96">
-                  <span className="font-semibold">Tempo de preparo: </span>
-                  {food.preparo}
-                </p>
-              </div>
+              <button
+                onClick={handleSubmit}
+                className="mx-auto w-1/4 mb-8 border-zinc-400 bg-zinc-200 p-3 font-bold rounded-full hover:bg-zinc-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-7  00  ">
+                Fazer Pedido
+              </button>
             </div>
-            <button
-              onClick={handleSubmit}
-              className="mx-auto w-1/4 mb-8 bg-black text-white p-3 font-bold rounded-full hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-7  00  ">
-              Fazer Pedido
-            </button>
           </div>
         </>
       )}
