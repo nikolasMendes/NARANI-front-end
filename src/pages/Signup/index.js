9e926019ec1d85e7f9880f532ef1195b48797fc7
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-03-09 09:36:11 -0300

    Sign Up css

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { api } from "../../api/api";
 import { useNavigate } from "react-router-dom";
-
+import sushihero from "../../images/sushihero.png";
 export function Signup() {
   const navigate = useNavigate();
   const [form, setForm] = useState({
@@ -44,41 +44,69 @@ export function Signup() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="formName">Nome:</label>
-      <input
-        id="formName"
-        name="name"
-        type="text"
-        value={form.name}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="formEmail">E-mail:</label>
-      <input
-        id="formEmail"
-        name="email"
-        type="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      <label htmlFor="formPassword">Senha:</label>
-      <input
-        id="formPassword"
-        name="password"
-        type="password"
-        value={form.password}
-        onChange={handleChange}
-      />
-      <label htmlFor="formConfirmPassword">Confirmação de senha</label>
-      <input
-        id="formConfirmPassword"
-        type="password"
-        name="confirmPassword"
-        value={form.confirmPassword}
-        onChange={handleChange}
-      />
-      <button type="submit">Cadastrar</button>
-    </form>
+    <div class="flex flex-col min-h-full items-center   bg-[#F9E4D4] h-screen pb-96">
+      <img src={sushihero} alt="signUp logo" className="w-80" />
+      <div className="mx-auto my-8 p-6 max-w-2xl border-4 border-gray-800 border-double rounded-md bg-[#e09e6e] shadow-2xl ">
+        <form onSubmit={handleSubmit} className="space-y-8 border-black-500">
+          <div className="mt-6 text-left ">
+            <label htmlFor="formName" className="font-semibold">
+              Nome:
+            </label>
+            <input
+              id="formName"
+              name="name"
+              type="text"
+              value={form.name}
+              onChange={handleChange}
+              class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="mt-6 text-left ">
+            <label htmlFor="formEmail" className="font-semibold">
+              E-mail:
+            </label>
+            <input
+              id="formEmail"
+              name="email"
+              type="email"
+              value={form.email}
+              onChange={handleChange}
+              class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="mt-6 text-left ">
+            <label htmlFor="formPassword" className="font-semibold">
+              Senha:
+            </label>
+            <input
+              id="formPassword"
+              name="password"
+              type="password"
+              value={form.password}
+              onChange={handleChange}
+              class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="mt-6 text-left ">
+            <label htmlFor="formConfirmPassword" className="font-semibold">
+              Confirmação de senha
+            </label>
+            <input
+              id="formConfirmPassword"
+              type="password"
+              name="confirmPassword"
+              value={form.confirmPassword}
+              onChange={handleChange}
+              class="relative block w-80 rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <button
+            type="submit"
+            class="place-self-center bg-[#f9e4d4] text-center font-bold border-orange-400 py-0.5 border-solid border w-1/3 ml-24 rounded-2xl pointer-events-auto">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
