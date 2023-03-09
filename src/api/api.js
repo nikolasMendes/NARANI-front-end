30ce4b812c1bfb1e437986a1a7664c35d1d46aac
nikolasMendes <nikolasmendes98@hotmail.com>
2023-03-09 13:24:07 -0300

    correcao rota

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000/api",
-  production: "https://narani.cyclic.app",
+  production: "https://narani.cyclic.app/api",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
